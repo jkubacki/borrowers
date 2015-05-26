05eba9648e3fefaa3cc64c416322889d4d552f4d
Jakub Kubacki <jakub.kubacki@netguru.co>
2015-05-26 22:47:39 +0200

    Friends new route with form

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   //this.route('friends');
   this.resource('friends', function() { });
-  this.route('friends', function() {});
+  this.route('friends', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
