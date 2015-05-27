0981c4ab703c3641986706fc7ab3c6a483ee35f8
Jakub Kubacki <jakub.kubacki@netguru.co>
2015-05-27 04:07:10 +0200

    Deactivate route hooks for friends

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,14 +4,10 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   },
-  actions: {
-    save: function(){
-      console.log('+-- save action bubbled up to friends new route');
-      return true;
-    },
-    cancel: function(){
-      console.log('+-- cancel action bubbled up to friends new route');
-      return true;
+  deactivate: function() {
+    var model = this.modelFor('friends/new');
+    if (model.get('isNew')) {
+      model.destroyRecord();
     }
   }
 });
